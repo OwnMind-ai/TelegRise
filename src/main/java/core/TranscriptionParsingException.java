d1c423658db64dc44244d7635e5c2c32d3aa04cc
OwnMind-ai <ownmind39@gmail.com>
2023-01-04 17:26:45 -0700

    Create TranscriptionParsingException.java

---
+++ src/main/java/core/TranscriptionParsingException.java
@@ -0,0 +1,29 @@
+package core;
+
+import org.w3c.dom.Node;
+
+public class TranscriptionParsingException extends Exception{
+    private final String message;
+    private final Node problematicNode;
+
+    public TranscriptionParsingException(String message, Node problematicNode) {
+        this.message = message;
+        this.problematicNode = problematicNode;
+    }
+
+    private String nodeTag(){
+        StringBuilder result = new StringBuilder("<" + problematicNode.getNodeName() + " ");
+
+        for (int i = 0; i < problematicNode.getAttributes().getLength(); i++) {
+            Node item = problematicNode.getAttributes().item(i);
+            result.append(item.getNodeName()).append("=\"").append(item.getTextContent()).append("\" ");
+        }
+
+        return result.append(">").toString();
+    }
+
+    @Override
+    public String toString() {
+        return "Transcription parsing error at :\n\n" + this.nodeTag() + "\n\n" + message;
+    }
+}
