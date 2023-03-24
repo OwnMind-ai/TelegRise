f0c686dfe23823c97f4e03debb7d0e1289f97255
OwnMind-ai <ownmind39@gmail.com>
2023-03-24 17:56:39 -0600

    feat: voice element

---
+++ src/main/java/org/telegram/telegrise/core/elements/media/Voice.java
@@ -0,0 +1,54 @@
+package org.telegram.telegrise.core.elements.media;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.send.SendMediaBotMethod;
+import org.telegram.telegrambots.meta.api.methods.send.SendVoice;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.elements.actions.Send;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+import java.util.List;
+
+@Element(name = "voice")
+@Data @NoArgsConstructor
+public class Voice implements MediaType{
+    @Attribute(name = "fileId")
+    private GeneratedValue<String> fileId;
+
+    @Attribute(name = "url")
+    private GeneratedValue<String> url;
+
+    @Attribute(name = "inputFile")
+    private GeneratedValue<InputFile> inputFile;
+
+    @Attribute(name = "duration")
+    private GeneratedValue<Integer> duration;
+
+    @Override
+    public SendMediaBotMethod<?> createSender(Send parent, ResourcePool pool) {
+        return SendVoice.builder()
+                .chatId(parent.generateChatId(pool))
+                .messageThreadId( generateNullableProperty(parent.getMessageThreadId(), pool))
+                .voice(this.createInputFile(pool))
+                .duration(generateNullableProperty(duration, pool))
+                .disableNotification( generateNullableProperty(parent.getDisableNotification(), pool))
+                .protectContent( generateNullableProperty(parent.getProtectContent(), pool))
+                .replyToMessageId( generateNullableProperty(parent.getReplyTo(), pool))
+                .caption(parent.getText() != null ? parent.getText().getText().generate(pool) : null)
+                .captionEntities(parent.getText() != null ? generateNullableProperty(parent.getText().getEntities(), List.of(), pool) : List.of())
+                .parseMode(parent.getText() != null ? generateNullableProperty(parent.getText().getParseMode(), pool) : null)
+                .allowSendingWithoutReply( generateNullableProperty(parent.getAllowSendingWithoutReply(), pool))
+                .replyMarkup(parent.createKeyboard(pool))
+                .build();
+    }
+
+    @Override
+    public List<InputMedia> createInputMedia(Send parent, ResourcePool pool) {
+        return null;
+    }
+}
