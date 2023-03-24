2ed3aa384611f9a9a0a4b6816c5cf18e3a02d57b
OwnMind-ai <ownmind39@gmail.com>
2023-03-24 17:49:51 -0600

    feat: document element

---
+++ src/main/java/org/telegram/telegrise/core/elements/media/Document.java
@@ -0,0 +1,58 @@
+package org.telegram.telegrise.core.elements.media;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
+import org.telegram.telegrambots.meta.api.methods.send.SendMediaBotMethod;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
+import org.telegram.telegrambots.meta.api.objects.media.InputMediaDocument;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.elements.actions.Send;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+import java.util.List;
+
+@Element(name = "document")
+@Data @NoArgsConstructor
+public class Document implements MediaType{
+    @Attribute(name = "fileId")
+    private GeneratedValue<String> fileId;
+
+    @Attribute(name = "url")
+    private GeneratedValue<String> url;
+
+    @Attribute(name = "inputFile")
+    private GeneratedValue<InputFile> inputFile;
+
+    @Attribute(name = "thumbnail")
+    private GeneratedValue<InputFile> thumbnail;
+
+    @Override
+    public SendMediaBotMethod<?> createSender(Send parent, ResourcePool pool) {
+        return SendDocument.builder()
+                .chatId(parent.generateChatId(pool))
+                .messageThreadId( generateNullableProperty(parent.getMessageThreadId(), pool))
+                .document(this.createInputFile(pool))
+                .thumb(generateNullableProperty(thumbnail, pool))
+                .disableNotification( generateNullableProperty(parent.getDisableNotification(), pool))
+                .protectContent( generateNullableProperty(parent.getProtectContent(), pool))
+                .replyToMessageId( generateNullableProperty(parent.getReplyTo(), pool))
+                .caption(parent.getText() != null ? parent.getText().getText().generate(pool) : null)
+                .captionEntities(parent.getText() != null ? generateNullableProperty(parent.getText().getEntities(), List.of(), pool) : List.of())
+                .parseMode(parent.getText() != null ? generateNullableProperty(parent.getText().getParseMode(), pool) : null)
+                .allowSendingWithoutReply( generateNullableProperty(parent.getAllowSendingWithoutReply(), pool))
+                .replyMarkup(parent.createKeyboard(pool))
+                .build();
+    }
+
+    @Override
+    public List<InputMedia> createInputMedia(Send parent, ResourcePool pool) {
+        InputMediaDocument mediaDocument = new InputMediaDocument();
+        mediaDocument.setThumb(generateNullableProperty(thumbnail, pool));
+
+        return List.of(this.createInputMedia(mediaDocument, pool));
+    }
+}
