627db5bfc193911a375b032c4fce9e8e12ca164e
OwnMind-ai <ownmind39@gmail.com>
2023-03-25 12:57:01 -0600

    feat: add Media & MediaGroup elements

---
+++ src/main/java/org/telegram/telegrise/core/elements/media/Media.java
@@ -0,0 +1,58 @@
+package org.telegram.telegrise.core.elements.media;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.objects.InputFile;
+import org.telegram.telegrambots.meta.api.objects.media.InputMedia;
+import org.telegram.telegrise.TelegRiseRuntimeException;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.elements.actions.Send;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+import java.util.List;
+
+@Element(name = "media")
+@Data @NoArgsConstructor
+public class Media implements MediaType{
+    @Attribute(name = "sendMethod")
+    private GeneratedValue<PartialBotApiMethod<?>> sendMethod;
+
+    @Attribute(name = "inputMedia")
+    private GeneratedValue<List<InputMedia>> inputMedia;
+
+    @Override
+    public PartialBotApiMethod<?> createSender(Send parent, ResourcePool pool) {
+        if (sendMethod == null)
+            throw new TelegRiseRuntimeException("Unable to send media: send method is null");
+
+        return sendMethod.generate(pool);
+    }
+
+    @Override
+    public List<InputMedia> createInputMedia(Send parent, ResourcePool pool) {
+        return inputMedia.generate(pool);
+    }
+
+    @Override
+    public GeneratedValue<String> getFileId() {
+        return null;
+    }
+
+    @Override
+    public boolean isGroupable() {
+        return inputMedia != null;
+    }
+
+    @Override
+    public GeneratedValue<String> getUrl() {
+        return null;
+    }
+
+    @Override
+    public GeneratedValue<InputFile> getInputFile() {
+        return null;
+    }
+}
