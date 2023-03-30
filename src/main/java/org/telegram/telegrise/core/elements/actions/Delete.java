322618fec16777838aa5f6a84d2e25e5d80f8ab3
OwnMind-ai <ownmind39@gmail.com>
2023-03-30 17:52:03 -0600

    feat: delete element

---
+++ src/main/java/org/telegram/telegrise/core/elements/actions/Delete.java
@@ -0,0 +1,28 @@
+package org.telegram.telegrise.core.elements.actions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+@Element(name = "delete")
+@Data @NoArgsConstructor
+public class Delete implements ActionElement{
+    @Attribute(name = "chat")
+    private GeneratedValue<Long> chatId;
+
+    @Attribute(name = "messageId", nullable = false)
+    private GeneratedValue<Integer> messageId;
+
+    @Override
+    public PartialBotApiMethod<?> generateMethod(ResourcePool resourcePool) {
+        return DeleteMessage.builder()
+                .chatId(this.generateChatId(resourcePool))
+                .messageId(messageId.generate(resourcePool))
+                .build();
+    }
+}
