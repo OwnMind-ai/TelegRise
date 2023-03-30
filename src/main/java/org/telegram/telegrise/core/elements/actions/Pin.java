00fe917980dc83adc5da7d60b5873e41f7c6bc87
OwnMind-ai <ownmind39@gmail.com>
2023-03-30 17:57:36 -0600

    feat: pin & unpin elements

---
+++ src/main/java/org/telegram/telegrise/core/elements/actions/Pin.java
@@ -0,0 +1,32 @@
+package org.telegram.telegrise.core.elements.actions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.pinnedmessages.PinChatMessage;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+@Element(name = "pin")
+@Data @NoArgsConstructor
+public class Pin implements ActionElement{
+    @Attribute(name = "chat")
+    private GeneratedValue<Long> chatId;
+
+    @Attribute(name = "messageId", nullable = false)
+    private GeneratedValue<Integer> messageId;
+
+    @Attribute(name = "disableNotification")
+    private GeneratedValue<Boolean> disableNotification;
+
+    @Override
+    public PartialBotApiMethod<?> generateMethod(ResourcePool resourcePool) {
+        return PinChatMessage.builder()
+                .chatId(this.generateChatId(resourcePool))
+                .messageId(messageId.generate(resourcePool))
+                .disableNotification(generateNullableProperty(disableNotification, resourcePool))
+                .build();
+    }
+}
---
+++ src/main/java/org/telegram/telegrise/core/elements/actions/Unpin.java
@@ -0,0 +1,38 @@
+package org.telegram.telegrise.core.elements.actions;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.telegram.telegrambots.meta.api.methods.PartialBotApiMethod;
+import org.telegram.telegrambots.meta.api.methods.pinnedmessages.UnpinAllChatMessages;
+import org.telegram.telegrambots.meta.api.methods.pinnedmessages.UnpinChatMessage;
+import org.telegram.telegrise.core.GeneratedValue;
+import org.telegram.telegrise.core.ResourcePool;
+import org.telegram.telegrise.core.parser.Attribute;
+import org.telegram.telegrise.core.parser.Element;
+
+@Element(name = "unpin")
+@Data
+@NoArgsConstructor
+public class Unpin implements ActionElement{
+    @Attribute(name = "chat")
+    private GeneratedValue<Long> chatId;
+
+    @Attribute(name = "messageId", nullable = false)
+    private GeneratedValue<Integer> messageId;
+
+    @Attribute(name = "all")
+    private GeneratedValue<Boolean> all = GeneratedValue.ofValue(false);
+
+    @Override
+    public PartialBotApiMethod<?> generateMethod(ResourcePool resourcePool) {
+        if (all.generate(resourcePool))
+            return UnpinAllChatMessages.builder()
+                    .chatId(this.generateChatId(resourcePool))
+                    .build();
+        else
+            return UnpinChatMessage.builder()
+                    .chatId(this.generateChatId(resourcePool))
+                    .messageId(messageId.generate(resourcePool))
+                    .build();
+    }
+}
